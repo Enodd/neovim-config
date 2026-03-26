2837c1ce6a60f1504cb95b5be81c430a44d2ec6d
Damian Nowak <foxnacity@gmail.com>
2026-03-26 21:25:32 +0100

    feat: add terminal theme brightness detection

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,22 +1,21 @@
+local theme_file = vim.fn.expand("~/.nvim_theme")
+local f = io.open(theme_file, "r")
+local theme = f and f:read("*l") or "dark"
+if f then
+  f:close()
+end
+
 return {
   {
     "sainnhe/everforest",
     lazy = false,
     priority = 1000,
     config = function()
+      vim.o.background = theme
       vim.g.everforest_background = "medium"
       vim.g.everforest_transparent_background = 2
       vim.g.everforest_better_performance = 1
       vim.cmd("colorscheme everforest")
     end,
   },
-  -- {
-  --   "catppuccin/nvim",
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   lazy = false,
-  --   config = function()
-  --     vim.cmd("colorscheme catppuccin")
-  --   end,
-  -- },
 }
